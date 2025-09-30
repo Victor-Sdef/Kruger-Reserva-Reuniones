70ef8c1bf175ab3b1fddc22d66b0276389a84687
VictorSdef <Alfonsovictor123castro@gmail.com>
2025-09-30 11:14:13 -0500

    init proyect

---
+++ empresa/src/test/java/com/kruger/reserva/empresa/EmpresaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kruger.reserva.empresa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpresaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
